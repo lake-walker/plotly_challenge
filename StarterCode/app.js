4a259624afbb0fb4dac41f9887b2f8d745542365
Lake Brant <lake.brant@du.edu>
2021-01-10 11:34:28 -0700

    started with d3 and have switched to plotly documentation

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,19 +1,40 @@
-// Read json file into js file and console log it
-d3.json('samples.json').then(function(data) {
-    // console.log(data[0]);
-    var names = data.names;
+// I believe this is the right way to do it.
+function getPlots(id) {
+  d3.json('samples.json').then(function(data) {
+    console.log(data);
+    var names = data.samples[0].otu_ids;
     console.log(names);
-    // Add options to the dropdown
-    d3.select('#selDataset')
-      .selectAll('myOptions')
-        .data(names)
-      .enter()
-        .append('option')
-      .text(function(d) {
-        return d;
-      })
-      .attr('value', function(d) { return d});
-});
+  })
+};
+
+getPlots();
+
+
+
+// Read json file into js file and console log it
+// d3.json('samples.json').then(function(data) {
+//     console.log(data);
+//     var names = data.names;
+//     var values = data.samples.sample_values;
+//     console.log(values);
+//     console.log(names);
+//     // Add options to the dropdown
+//     d3.select('#selDataset')
+//       .selectAll('myOptions')
+//         .data(names)
+//       .enter()
+//         .append('option')
+//       .text(function(d) {
+//         return d;
+//       })
+//       .attr('value', function(d) { return d});
+// });
+
+// var data = [
+//   {
+//     x: sample_values,
+//   }
+// ]
 
 // var select = document.getElementById('selDataset');
 // for(index in names) {
