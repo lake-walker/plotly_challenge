d93c07c1304bfd86b788244e0053d7461c931213
Lake Brant <lake.brant@du.edu>
2021-01-10 12:04:47 -0700

    first attempt at bubble chart

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -2,8 +2,70 @@
 function getPlots(id) {
   d3.json('samples.json').then(function(data) {
     console.log(data);
-    var names = data.samples[0].otu_ids;
-    console.log(names);
+    var ids = data.samples[0].otu_ids;
+    console.log(ids);
+    var values = data.samples[0].sample_values;
+    console.log(values);
+    var topValues = data.samples[0].sample_values.slice(0,10).reverse();
+    console.log(topValues);
+    var labels = data.samples[0].otu_labels;
+    console.log(labels);
+    var topLabels = data.samples[0].otu_labels.slice(0,10);
+    console.log(topLabels);
+    // Top ten bacteria ids
+    var top_ids = (data.samples[0].otu_ids.slice(0,10)).reverse();
+    console.log(top_ids);
+    // Reformat the ids
+    var otuID = top_ids.map(d => 'OTU' + d);
+    console.log(otuID);
+    // Create traces for ids
+    var trace = {
+      x: topValues,
+      y: otuID,
+      text: topLabels,
+      color: 'red',
+      type: 'bar',
+      orientation:'h',
+    };
+    var data = [trace];
+
+    var layout = {
+      title: 'Top 10 OTU',
+      yaxis:{tickmode:'linear'},
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 30
+    }
+    };
+    Plotly.newPlot('bar',data,layout);
+
+    // Create the bubble chart
+    var trace1 = {
+      x: ids,
+      y: values,
+      mode: 'markers',
+      marker: {
+        size: values,
+        color: ids,
+      },
+      text: labels,
+    };
+
+    var data1 = [trace1];
+
+    var layout1 = {
+      title: 'OTU Data',
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 30
+      }
+    };
+    Plotly.newPlot('myDiv', data1, layout1);
+
   })
 };
 
